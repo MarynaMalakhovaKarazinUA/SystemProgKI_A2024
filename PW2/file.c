c2b0287d2a83660f16565ba268354119070a2299
Olena Plakhotia <olena.plakhotia@student.karazin.ua>
2024-10-11 14:16:31 +0300

    Creating a directory

--- PW2/file.c
+++ PW2/file.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
-    // Define the name of the file to be created
-    const char *file_name = "new_file.txt";
+    const char *path = "/app/new_directory";  // Назва каталогу
+    int status = mkdir(path, 0755);  // Створення каталогу з правами доступу
 
-    // Attempt to open the file in write mode
-    FILE *file = fopen(file_name, "w");
-    
-    // Check if the file was successfully created
-    if (file == NULL) {
-        perror("Error creating file"); // Print error message using perror
-        return 1; // Return a non-zero value to indicate failure
+    if (status == 0) {
+        printf("Каталог успішно створено.\n");
+    } else {
+        perror("Помилка при створенні каталогу");
     }
 
-    // Write a message to the newly created file
-    fprintf(file, "Hello, this is a new file!\n");
-
-    // Close the file to ensure data is written properly
-    fclose(file);
-
-    // Notify the user of successful file creation
-    printf("File '%s' has been successfully created in the current directory.\n", file_name);
-    
-    return 0; // Return zero to indicate success
+    return 0;
 }
